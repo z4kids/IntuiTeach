15710dd3de2bfbe2006cdc93c4a7db4e003e2d23
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-08-08 17:59:19 -0400

    updated api for new database schema

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const client = require('../db/connection.js')
 const {ObjectId} = require('mongodb')
 
-
+DB_NAME = "z4kidz"
 // API for teacher to add new question
 router.post('/new_question', async (req, res) => {
     await client.connect()
     // Deconstructs the prompt of the question, the list of the answers, the correct answer, and the max time to answer the question
-    const {prompt, list_of_answers, correct_answer, max_time} = req.body
+    const {prompt, list_of_answers, correct_answer, max_time, points, teacher_id, exam_id} = req.body
 
     // writing JSON file to pass to Mongo client, making sure it is in the same JSON format on MongoDB database 
     const question = {
@@ -16,10 +16,20 @@ router.post('/new_question', async (req, res) => {
         options: list_of_answers,
         correct: correct_answer,
         max_time,
+        points
     }
 
     // inserts the new question and its parameters 
-    const result = await client.db("z4kidz").collection("question").insertOne(question)
+    const result = await client.db(DB_NAME).collection("question").insertOne(question)
+
+    // Updates the teacher's list of asked quesitons
+
+    const teacher = await client.db(DB_NAME).collection("exam").findOne({teacher_id:ObjectId(teacher_id)})
+
+    const new_teacher_question = {
+      questions: (teacher.questions) ? [...teacher.questions, ObjectId(String(result.insertedId))] : [ObjectId(String(result.insertedId))],
+    }
+    await client.db(DB_NAME).collection("exam").updateOne({_id: ObjectId(exam_id)}, {$set: new_teacher_question})
 
     console.log(`New question created with the following id: ${result.insertedId}`);
     res.sendStatus(200)
@@ -29,7 +39,7 @@ router.post('/new_question', async (req, res) => {
 router.post('/delete_question', async (req, res) => {
     await client.connect()
     // Deconstructs the question id of the question the teacher wants deleted 
-    const {question_id} = req.body
+    const {question_id, exam_id} = req.body
 
     // Constructs JSON file to pass into client 
     const question_deleted = {
@@ -37,7 +47,9 @@ router.post('/delete_question', async (req, res) => {
     }
 
     // Finds the quesiton id sent by the request and deletes it 
-    const result = await client.db("z4kidz").collection("question").deleteOne(question_deleted)
+    const result = await client.db(DB_NAME).collection("question").deleteOne(question_deleted)
+
+    await client.db(DB_NAME).collection("exam").findOneAndUpdate({_id: ObjectId(exam_id)}, {$pull: {questions: ObjectId(question_id)}})
 
     console.log(`${result.deletedCount} document(s) was/were deleted.`)
     res.sendStatus(200)
