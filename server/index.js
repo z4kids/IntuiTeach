a60677b4106ec32e88cbbcc854107ce7e1ae4f0a
Ryan Wolf <ryantwolf1@gmail.com>
2020-08-07 21:04:45 -0400

    Add files for routers

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
-console.log('Hello World!')
+const express = require('express')
+const teacher = require('./routes/teacher')
+const student = require('./routes/student')
+const app = express();
+
+const PORT = 3000
+
+app.all('/teacher', teacher)
+app.all('/student', student)
+
+app.listen(PORT)
