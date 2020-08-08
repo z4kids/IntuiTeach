59743fc5700f2b6aa31a5b0338343471c85015ac
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-08-07 22:38:02 -0400

    fixed stuff

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,32 +1,5 @@
 const {MongoClient} = require('mongodb')
-require("dotenv").config()
-
-async function listDatabases(client){
-    databasesList = await client.db().admin().listDatabases();
- 
-    console.log("Databases:");
-    databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
-
-async function main(){
-    // const uri = `mongodb+srv://${process.env.DB_USERNAME}:<${process.env.PASSWORD}>@${process.env.DB_CLUSTER_URL}/<dbname>?retryWrites=true&w=majority`
-    console.log(process.env.DB_URI)
-    const uri = process.env.DB_URI
-
-    const client = new MongoClient(uri);
- 
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
- 
-        // Make the appropriate DB calls
-        await  listDatabases(client);
- 
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-
-main().catch(console.error);
+const uri = process.env.DB_URI
+const client = new MongoClient(uri)
+await client.connect()
+module.exports = client
--- server/index.js
+++ server/index.js
@@ -6,21 +6,10 @@ const app = express();
 
 const PORT = 3000
 
-<<<<<<< HEAD
-app.use('/teacher/', teacher)
-app.use('/student/', student)
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-app.listen(PORT, () => { 
-    console.log(`Example app listening at http://localhost:${PORT}`)
-=======
 app.use('/teacher', teacher)
 app.use('/student', student)
 
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
->>>>>>> 1cdf92efd23a517320cc2dfac282010534db2d05
 })
