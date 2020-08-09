a7e45a308e2670342fd7f5daa736b790b24fd04b
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-08-08 21:56:59 -0400

    configure stats router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const teacher = require('./routes/teacher')
 const student = require('./routes/student')
+const stats = require('./routes/stats')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const app = express();
@@ -10,6 +11,7 @@ const PORT = 3000
 app.use(bodyParser.json())
 app.use('/teacher', teacher)
 app.use('/student', student)
+app.use('/stats', stats)
 
 
 app.listen(PORT, () => {
