0cb3f5fff24a16dbd0a369625aa7b1f1116a1623
Kshitij Sinha <kshitij.avi.s@gmail.com>
2020-08-11 16:32:00 -0500

    Fixed bug:Question lock after "Add question" click

--- client/src/components/Quizmaker/Components/QuestionForm.js
+++ client/src/components/Quizmaker/Components/QuestionForm.js
@@ -4,38 +4,40 @@ import AnswerChoice from './AnswerChoice'
 
 // Props: number, addQuestion, completed
 const QuestionForm = (props) => {
+    const number = props.number;
+
     function handleAddClick(e) {
         e.preventDefault();
         props.addQuestion();
-        document.getElementById('form-fields').disabled = true;
+        document.getElementById(`form-fields-${number}`).disabled = true;
     }
-
+    
     return (
         <Form className='question-form'>
-            <fieldset id='form-fields'>
-            <h2>Question {props.number}</h2>
-            <Form.Group>
-                <Form.Label srOnly>Enter a Question</Form.Label>
-                <Form.Control type='text' placeholder='Enter a Question'/>
-            </Form.Group>
-            <Row>
-                <Col>
-                    <AnswerChoice/>
-                </Col>
-                <Col>
-                    <AnswerChoice/>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <AnswerChoice/>
-                </Col>
-                <Col>
-                    <AnswerChoice/>
-                </Col>
-            </Row>
-            <Button as='input' type='submit' onClick={handleAddClick} value='Add Question' variant='primary' size='lg'/>
-            </fieldset>
+            <fieldset id={`form-fields-${number}`}>
+                <h2>Question {number}</h2>
+                <Form.Group>
+                    <Form.Label srOnly>Enter a Question</Form.Label>
+                    <Form.Control type='text' placeholder='Enter a Question'/>
+                </Form.Group>
+                <Row>
+                    <Col>
+                        <AnswerChoice/>
+                    </Col>
+                    <Col>
+                        <AnswerChoice/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <AnswerChoice/>
+                    </Col>
+                    <Col>
+                        <AnswerChoice/>
+                    </Col>
+                </Row>
+                <Button as='input' type='submit' onClick={handleAddClick} value='Add Question' variant='primary' size='lg'/>
+                </fieldset>
         </Form>
     )
 }
