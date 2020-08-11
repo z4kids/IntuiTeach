c566ee8840b5a088e59e8100ea0cc0a9cabd5d62
Kshitij Sinha <kshitij.avi.s@gmail.com>
2020-08-11 00:57:15 -0500

    Add button control component

--- client/src/Quizmaker.js
+++ client/src/Quizmaker.js
@@ -3,33 +3,22 @@ import { Nav, Button, ButtonToolbar, Form, Row, Col, FormLabel} from "react-boot
 import { withRouter } from "react-router";
 import './Quizmaker.css'
 
-const Quizmaker = props => {
-    return (
-        <div>
-            <ButtonToolbar  className='quiz-controls' aria-label="Quiz controls">
-                <Button variant='light' size='lg'>Add Question</Button>
-                <Button variant='primary' size='lg'>Save</Button>
-            </ButtonToolbar>
-            <div className='question-list'>
-                <QuestionForm/>
-            </div>
-            
-        </div>
-    );
-};
+const ButtonControls = (props) => (
+    <ButtonToolbar  className='quiz-controls' aria-label="Quiz controls">
+        <Button variant='light' size='lg'>Add Question</Button>
+        <Button variant='primary' size='lg'>Save</Button>
+    </ButtonToolbar>
+)
 
-const AnswerChoice = (props) => {
-    return (
-        <Form.Group>
-            <Form.Label srOnly>Enter an Answer Choice</Form.Label>
-            <Form.Control type='text' placeholder='Possible Answer'/>
-        </Form.Group>
-    )
-}
-
+const AnswerChoice = (props) => (
+    <Form.Group>
+        <Form.Label srOnly>Enter an Answer Choice</Form.Label>
+        <Form.Control type='text' placeholder='Possible Answer'/>
+    </Form.Group>
+)
 const QuestionForm = (props) => (
     <Form className='question-form'>
-        <h2>New Question</h2>
+        <h2>Question {props.number}</h2>
         <Form.Group>
             <Form.Label srOnly>Enter a Question</Form.Label>
             <Form.Control type='text' placeholder='Enter a Question'/>
@@ -52,4 +41,21 @@ const QuestionForm = (props) => (
         </Row>
     </Form>
 )
+
+const questions = [{ number: 1, key:'form-1' }, { number: 2, key: 'form-2'}];
+const questionList = questions.map((question) => (
+    <QuestionForm 
+        number={question.number}
+        key={question.key} />
+))
+const Quizmaker = props => {
+    return (
+        <div>
+            <ButtonControls/>
+            <div className='question-list'>
+                {questionList}
+            </div>
+        </div>
+    );
+};
 export default Quizmaker;
