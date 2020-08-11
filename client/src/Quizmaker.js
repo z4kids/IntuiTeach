1e5d59e073bd49b26988af0e4e2658cbc08f0d24
Kshitij Sinha <kshitij.avi.s@gmail.com>
2020-08-11 01:37:08 -0500

    Use props to past info from Button to Quizmaker

--- client/src/Quizmaker.js
+++ client/src/Quizmaker.js
@@ -4,13 +4,13 @@ import { withRouter } from "react-router";
 import './Quizmaker.css'
 
 const ButtonControls = (props) => {    
-    function handleClick(e) {
+    function handleAddClick(e) {
         e.preventDefault();
-        alert('Hello, world!');
+        props.addQuestion();
     }
     return (
         <ButtonToolbar  className='quiz-controls' aria-label="Quiz controls">
-            <Button as='input' type='submit' onClick={handleClick} value='Add Question' variant='light' size='lg'/>
+            <Button as='input' type='submit' onClick={handleAddClick} value='Add Question' variant='light' size='lg'/>
             <Button variant='primary' size='lg'>Save</Button>
         </ButtonToolbar>
     )
@@ -22,32 +22,37 @@ const AnswerChoice = (props) => (
         <Form.Control type='text' placeholder='Possible Answer'/>
     </Form.Group>
 )
-const QuestionForm = (props) => (
-    <Form className='question-form'>
-        <h2>Question {props.number}</h2>
-        <Form.Group>
-            <Form.Label srOnly>Enter a Question</Form.Label>
-            <Form.Control type='text' placeholder='Enter a Question'/>
-        </Form.Group>
-        <Row>
-            <Col>
-                <AnswerChoice/>
-            </Col>
-            <Col>
-                <AnswerChoice/>
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-                <AnswerChoice/>
-            </Col>
-            <Col>
-                <AnswerChoice/>
-            </Col>
-        </Row>
-    </Form>
-)
+const QuestionForm = (props) => {
+    return (
+        <Form className='question-form'>
+            <h2>Question {props.number}</h2>
+            <Form.Group>
+                <Form.Label srOnly>Enter a Question</Form.Label>
+                <Form.Control type='text' placeholder='Enter a Question'/>
+            </Form.Group>
+            <Row>
+                <Col>
+                    <AnswerChoice/>
+                </Col>
+                <Col>
+                    <AnswerChoice/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <AnswerChoice/>
+                </Col>
+                <Col>
+                    <AnswerChoice/>
+                </Col>
+            </Row>
+        </Form>
+    )
+}
 
+function addQuestion() {
+    alert('Question added');
+}
 const questions = [{ number: 1, key:'form-1' }, { number: 2, key: 'form-2'}];
 const questionList = questions.map((question) => (
     <QuestionForm 
@@ -57,7 +62,7 @@ const questionList = questions.map((question) => (
 const Quizmaker = props => {
     return (
         <div>
-            <ButtonControls/>
+            <ButtonControls addQuestion={addQuestion}/>
             <div className='question-list'>
                 {questionList}
             </div>
