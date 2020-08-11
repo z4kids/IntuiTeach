7e43d0ff4f903b067211006dcca5f9c333607a21
Kshitij Sinha <kshitij.avi.s@gmail.com>
2020-08-11 02:04:51 -0500

    Add question btn now adds new identical questions

--- client/src/Quizmaker/Quizmaker.js
+++ client/src/Quizmaker/Quizmaker.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Nav, Button, ButtonToolbar, Form, Row, Col, FormLabel} from "react-bootstrap";
 import { withRouter } from "react-router";
 import './Quizmaker.css'
 import ButtonControls from './Components/ButtonControls'
 import QuestionForm from './Components/QuestionForm'
 
-
-function addQuestion() {
-    alert('Question added');
-}
-const questions = [{ number: 1, key:'form-1' }, { number: 2, key: 'form-2'}];
-const questionList = questions.map((question) => (
+const Quizmaker = props => {
+    function addQuestion() {
+        const newQuestion = { number: 1, key: 'form 1'}
+        setQuestions([...questions, newQuestion])
+    }
+    const firstQuestion = [{ number: 1, key:'form-1' }];
+    const [questions, setQuestions] = useState(firstQuestion);
+    const questionList = questions.map((question) => (
     <QuestionForm 
         number={question.number}
-        key={question.key} />
-))
+        key={question.key}/>
+    ))
 
-const Quizmaker = props => {
     return (
         <div>
             <ButtonControls addQuestion={addQuestion}/>
