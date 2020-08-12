f3d2cf9660ed5431afc6fcaf236a918866088948
AzureQDragon <darthmaul0310@gmail.com>
2020-08-11 20:36:40 -0400

    Add buggy rewards system

--- client/src/components/Quizmaker/Quizmaker.js
+++ client/src/components/Quizmaker/Quizmaker.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { Nav } from "react-bootstrap";
 import { withRouter } from "react-router";
-import { Row, Col, Container } from 'react-bootstrap';
+import { Form, Row, Col, Container } from 'react-bootstrap';
 import './Quizmaker.css'
 import ButtonControls from './Components/ButtonControls'
 import QuestionForm from './Components/QuestionForm'
 import Sidebar from '../../components/sidebar.js';
-
+import Rewards from '../Rewards/rewards.js'
 let questionNum = 2;
 
 const Quizmaker = props => {
@@ -39,11 +39,15 @@ const Quizmaker = props => {
                         <Sidebar />
                     </Col>
                     <Col xs={11} className="scroll">
+                        
+                        
                         <div className='question-maker'>
+                            <ButtonControls addQuestion={addQuestion}/>
                             <div className='question-list'>
                                 {questionList}
                             </div>
                         </div>
+                        <Rewards />
                     </Col>
                 </Row>
 
