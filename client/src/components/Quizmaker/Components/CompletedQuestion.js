fdebca992e5198270720c2b7275263840590128b
Kshitij Sinha <kshitij.avi.s@gmail.com>
2020-08-13 13:59:05 -0500

    Fixed quiz-viewer formatting issues

--- client/src/components/Quizmaker/Components/CompletedQuestion.js
+++ client/src/components/Quizmaker/Components/CompletedQuestion.js
@@ -9,28 +9,26 @@ const CompletedQuestion = (props) => {
     }
     
     return (
-        <div className = 'question-viewer' id={props.id}>
-            <Container className='question-form'>
-                <h3>{`Question ${props.number}: ${props.questionVal}`}</h3>
-                <Row>
-                    <Col>
-                        <ul>
-                            <li>{`${props.answer1Val}`}</li>
-                            <li>{`${props.answer2Val}`}</li>
-                        </ul>
-                    </Col>
-                    <Col>
-                        <ul>
-                            <li>{`${props.answer3Val}`}</li>
-                            <li>{`${props.answer4Val}`}</li>
-                        </ul>
-                    </Col>
-                </Row>
-            <div>
-                <Button as='input' type='button' onClick={handleDeleteClick} value='Delete question' variant='danger'/>
-            </div>
-            </Container>
+        <Container fluid className = 'question-viewer'>
+            <h3>{`Question ${props.number}: ${props.questionVal}`}</h3>
+            <Row>
+                <Col>
+                    <ul>
+                        <li>{`${props.answer1Val}`}</li>
+                        <li>{`${props.answer2Val}`}</li>
+                    </ul>
+                </Col>
+                <Col>
+                    <ul>
+                        <li>{`${props.answer3Val}`}</li>
+                        <li>{`${props.answer4Val}`}</li>
+                    </ul>
+                </Col>
+            </Row>
+        <div>
+            <Button as='input' type='button' onClick={handleDeleteClick} value='Delete question' variant='danger'/>
         </div>
+        </Container>
     )
 }
 
