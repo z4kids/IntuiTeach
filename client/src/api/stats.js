1c57c1b53bfe0bd1f1f4ddbcdb5227a7be63dd35
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-08-13 16:36:20 -0400

    added stats in JSON

--- client/src/api/stats.js
+++ client/src/api/stats.js
@@ -2,23 +2,27 @@ import {getStatsByExam, getStatsForAllStudents} from "./link.js"
 
 
 export async function calculateStats() {
-    const stats_jsons = await getStatsByExam("5f2f1e595982a2cd57b831a5");
+    const stats_jsons = await getStatsByExam()
 
     let times = []
     let answers = []
     let student_with_least_points = 1000
+    let longest_time = 0
+    let student_longest_time_on_exam
 
     stats_jsons.forEach(json => {
         for (let time of json.times) {
             times.push(time)
-        }
+            if (time > longest_time) {
+                longest_time = time
+                student_longest_time_on_exam = json.student_name
 
-        for (let answer of json.answers) {
-            answers.push(answer)
+            }
         }
         if (json.points < student_with_least_points) {
             student_with_least_points = json.student_name
         }
+
     })
 
     let average_time_for_exam = 0
@@ -30,6 +34,7 @@ export async function calculateStats() {
     average_time_for_exam /= times.length
 
     console.log(average_time_for_exam)
+    console.log(student_longest_time_on_exam)
 
     let most_missed;
 
@@ -38,7 +43,10 @@ export async function calculateStats() {
 
     const stats = {
         average_time_for_exam: average_time_for_exam,
-
+        student_longest_time_on_exam: student_longest_time_on_exam,
+        student_with_least_points: student_with_least_points,
     }
+
+    return JSON.stringify(stats)
 }
 
