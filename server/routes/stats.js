854bfaa6580532c893120dcbaf9eebdeae463c47
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-08-13 17:51:55 -0400

    addes student names and question prompts to exam api call

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -16,17 +16,33 @@ router.get('/exam', async (req, res) => {
 
     let stats_exam = []
     results.forEach(stat => {
-        delete stat.exam_id
         stats_exam.push(stat)
     })
+
+    let correct_answers = []
+    let question_names = []
+
     for (let i = 0; i < results.length; i++) {
         s_id = stats_exam[i].student_id
-        q_id = stats_exam[i].question_id
+        e_id = stats_exam[i].exam_id
         let student = await client.db(DB_NAME).collection("student").findOne({_id: s_id})
+        let exam = await client.db(DB_NAME).collection("exam").findOne({_id: e_id})
+
+        for (let q_id of exam.questions) {
+            let question = await client.db(DB_NAME).collection("question").findOne({_id: q_id})
+            if (!(correct_answers.includes(question.correct))) {
+                correct_answers.push(question.correct)
+            }
+            if (!(question_names.includes(question.prompt))) {
+                question_names.push(question.prompt)
+            }
+        }
 
         stats_exam[i] = {
             ...stats_exam[i],
-            student_name: student.name
+            student_name: student.name,
+            correct_answers: correct_answers,
+            question_names: question_names
         }
     }
 
