6e7c3fc9e30a8379fb6f7cb15e5790a665fcc4b9
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-08-13 17:19:52 -0400

    added average percentage correct for exam

--- client/src/api/stats.js
+++ client/src/api/stats.js
@@ -2,13 +2,15 @@ import {getStatsByExam, getStatsForAllStudents} from "./link.js"
 
 
 export async function calculateStats() {
-    const stats_jsons = await getStatsByExam()
+    const stats_jsons = await getStatsByExam("5f2f1e595982a2cd57b831a5")
 
     let times = []
     let answers = []
     let student_with_least_points = 1000
     let longest_time = 0
     let student_longest_time_on_exam
+    let total_correct = 0
+    let correct_answers = []
 
     stats_jsons.forEach(json => {
         for (let time of json.times) {
@@ -23,8 +25,22 @@ export async function calculateStats() {
             student_with_least_points = json.student_name
         }
 
-    })
+        for (let correct_answer of json.correct_answers) {
+            if (!(correct_answers.includes(correct_answer))) {
+                correct_answers.push(correct_answer)
+            }
+        }
+        for (let answer of json.answers) {
+            answers.push(answer)
+            if (correct_answers.includes(answer)) {
+                total_correct += 1
+            }
+        }
 
+    })
+    console.log(answers)
+    console.log(correct_answers)
+    let average_percentage_correct_for_exam  = (total_correct / answers.length) * 100 + "%"
     let average_time_for_exam = 0
 
     for (let time of times) {
@@ -35,10 +51,7 @@ export async function calculateStats() {
 
     console.log(average_time_for_exam)
     console.log(student_longest_time_on_exam)
-
-    let most_missed;
-
-
+    console.log(average_percentage_correct_for_exam)
 
 
     const stats = {
@@ -47,6 +60,6 @@ export async function calculateStats() {
         student_with_least_points: student_with_least_points,
     }
 
-    return JSON.stringify(stats)
+    //return JSON.stringify(stats)
 }
 
