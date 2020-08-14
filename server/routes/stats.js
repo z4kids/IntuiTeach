d3d8a561ca5192d1d3da18b84a9f4df1ac074308
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-08-14 03:05:04 -0400

    added documentation and added new data retrieval for retrieve stats by students

--- server/routes/stats.js
+++ server/routes/stats.js
@@ -5,6 +5,7 @@ const {ObjectId} = require('mongodb')
 
 DB_NAME = "z4kidz"
 
+// API to retrieve all the stats for a specific exam
 router.get('/exam', async (req, res) => {
     await client.connect()
 
@@ -49,22 +50,48 @@ router.get('/exam', async (req, res) => {
     res.json(stats_exam)
 })
 
+// API to get all the stats of teacher's students
 router.get('/student', async (req, res) =>  {
     await client.connect()
 
-    const teacher = await client.db(DB_NAME).collection("teacher").findOne({zoom_id: req.session.user.id})
+    const teacher = await client.db(DB_NAME).collection("teacher").findOne({zoom_id: "jo2i343oajfdapb"})
 
     let stats = []
+    let correct_answers = []
+    let question_names = []
 
     for (let student_id of teacher.students) {
-        console.log(student_id)
-        let student_stats = await client.db(DB_NAME).collection("stats").find({student_id: student_id, teacher_id: ObjectId(teacher_id)})
+        let student_stats = await client.db(DB_NAME).collection("stats").find({student_id: student_id, teacher_id: ObjectId(teacher._id)})
 
         student_stats = await student_stats.toArray()
 
         student_stats.forEach(stat => {
             stats.push(stat)
             })
+
+            for (let i = 0; i < student_stats.length; i++) {
+                s_id = stats[i].student_id
+                e_id = stats[i].exam_id
+                let student = await client.db(DB_NAME).collection("student").findOne({_id: s_id})
+                let exam = await client.db(DB_NAME).collection("exam").findOne({_id: e_id})
+        
+                for (let q_id of exam.questions) {
+                    let question = await client.db(DB_NAME).collection("question").findOne({_id: q_id})
+                    if (!(correct_answers.includes(question.correct))) {
+                        correct_answers.push(question.correct)
+                    }
+                    if (!(question_names.includes(question.prompt))) {
+                        question_names.push(question.prompt)
+                    }
+                }
+        
+                stats[i] = {
+                    ...stats[i],
+                    student_name: student.name,
+                    correct_answers: correct_answers,
+                    question_names: question_names
+                }
+            }
         
     }
     res.json(stats)
